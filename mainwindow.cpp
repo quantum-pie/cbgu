62f0b629bb6435c04165322ef7d2401812b0220a
quantum-pie <big.awesome.pie@gmail.com>
2018-09-03 08:05:09 +0300

    Remove irrelevant code from MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include "ingredient.h"
-#include "meal.h"
-
-#include <iostream>
-#include <memory>
+#include "ingredientsdialog.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,22 +9,8 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    auto milk = std::make_shared<Ingredient>( "milk", 1, 2, 3, 4 );
-    auto bread = std::make_shared<Ingredient>( "bread", 5, 7, 5, 3 );
-
-    std::vector<std::shared_ptr<const AbstractProduct>> basket{ milk, bread };
-    Meal milk_with_bread{ "milk_with_bread", std::move(basket) };
-
-    std::cout  << milk_with_bread.get_name() << std::endl
-               << milk_with_bread.get_calories() << std::endl
-               << milk_with_bread.get_proteins() << std::endl
-               << milk_with_bread.get_fats() << std::endl
-               << milk_with_bread.get_carbs() << std::endl;
-
-    for(auto ingredient : milk_with_bread.get_ingredients())
-    {
-        std::cout << ingredient->get_name() << std::endl;
-    }
+    ingredients_dialog = new IngredientsDialog{ this };
+    ingredients_dialog->show();
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -7,16 +7,19 @@ namespace Ui {
 class MainWindow;
 }
 
+class IngredientsDialog;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    IngredientsDialog *ingredients_dialog;
 };
 
 #endif // MAINWINDOW_H
