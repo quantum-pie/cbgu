444d8cf713202a11787eb8ebf9dbd05940206bd6
quantum-pie <big.awesome.pie@gmail.com>
2018-09-03 18:00:33 +0300

    Implement insert and find in ProductDictionary

--- productdictionary.cpp
+++ productdictionary.cpp
@@ -1,11 +1,18 @@
 #include "productdictionary.h"
+#include "abstractproduct.h"
 
 AbstractProduct * ProductDictionary::find(const std::string & name)
 {
-    return nullptr;
+    auto it = dict_backend.find(name);
+    if(it != dict_backend.end())
+    {
+        return it->second;
+    }
+    else return nullptr;
 }
 
 bool ProductDictionary::insert(AbstractProduct * new_product)
 {
-    return false;
+    auto res = dict_backend.emplace(new_product->get_name(), new_product);
+    return res.second;
 }
