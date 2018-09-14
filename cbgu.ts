dcae4d11d2840baf74d3a980cf899453c8dfd02e
quantum-pie <big.awesome.pie@gmail.com>
2018-09-14 16:37:09 +0300

    Update translations

--- cbgu.ts
+++ cbgu.ts
@@ -2,6 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>BulletinItem</name>
+    <message>
+        <location filename="bulletinitem.ui" line="14"/>
+        <source>Form</source>
+        <translation>Whatever</translation>
+    </message>
+</context>
+<context>
     <name>GoalDialog</name>
     <message>
         <location filename="goaldialog.ui" line="14"/>
@@ -114,105 +122,110 @@
         <translation>Энергетическая ценность</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="42"/>
+        <location filename="mainwindow.ui" line="195"/>
         <source>History</source>
         <translation>Рацион</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="51"/>
+        <location filename="mainwindow.ui" line="204"/>
         <source>Meal:</source>
         <translation>Прием пищи:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="71"/>
+        <location filename="mainwindow.ui" line="42"/>
         <source>Summary</source>
         <translation>Итого</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="77"/>
+        <location filename="mainwindow.ui" line="81"/>
         <source>Calories:</source>
         <translation>Калории:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="96"/>
-        <location filename="mainwindow.ui" line="129"/>
-        <location filename="mainwindow.ui" line="162"/>
-        <location filename="mainwindow.ui" line="195"/>
+        <location filename="mainwindow.ui" line="60"/>
+        <location filename="mainwindow.ui" line="100"/>
+        <location filename="mainwindow.ui" line="126"/>
+        <location filename="mainwindow.ui" line="173"/>
         <source>/</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="110"/>
+        <location filename="mainwindow.ui" line="107"/>
         <source>Proteins:</source>
         <translation>Белки:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="143"/>
+        <location filename="mainwindow.ui" line="67"/>
         <source>Fats:</source>
         <translation>Жиры:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="176"/>
+        <location filename="mainwindow.ui" line="140"/>
         <source>Carbohydrates:</source>
         <translation>Углеводы:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="213"/>
+        <location filename="mainwindow.ui" line="183"/>
         <source>Goals</source>
-        <translation>Ежедневные цели</translation>
+        <translation>События</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="253"/>
+        <location filename="mainwindow.ui" line="225"/>
+        <source>Bulletin</source>
+        <translation>Сводка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="261"/>
         <source>Library</source>
         <translation>Библиотека</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="275"/>
+        <location filename="mainwindow.ui" line="283"/>
         <source>Ingredients</source>
         <translation>Ингредиенты</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="284"/>
+        <location filename="mainwindow.ui" line="292"/>
         <source>Meals</source>
         <translation>Блюда</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="41"/>
+        <location filename="mainwindow.cpp" line="42"/>
         <source>Breakfast</source>
         <translation>Завтрак</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="41"/>
+        <location filename="mainwindow.cpp" line="42"/>
         <source>Lunch</source>
         <translation>Обед</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="41"/>
+        <location filename="mainwindow.cpp" line="42"/>
         <source>Dinner</source>
         <translation>Ужин</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="41"/>
+        <location filename="mainwindow.cpp" line="42"/>
         <source>Snack</source>
         <translation>Перекус</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="71"/>
+        <location filename="mainwindow.cpp" line="77"/>
         <source>Add Product</source>
         <translation>Добавить продукт</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="72"/>
+        <location filename="mainwindow.cpp" line="78"/>
         <source>Remove Product</source>
         <translation>Удалить продукт</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="83"/>
+        <location filename="mainwindow.cpp" line="89"/>
         <source>Add Goal</source>
         <translation>Добавить цель</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="84"/>
+        <location filename="mainwindow.cpp" line="90"/>
         <source>Remove Goal</source>
         <translation>Удалить цель</translation>
     </message>
